d6a9a2f8c18f75d636034eac12c3afc47dde57ab
Renjith <renjith.baby@digicatapult.org.uk>
2024-07-19 10:04:34 +0100

    add additional env vars, remove defaults for env vars

--- dags/regression_model_training/train_dag.py
+++ dags/regression_model_training/train_dag.py
@@ -3,32 +3,41 @@
 import os
 
 from airflow.decorators import dag
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import \
-    KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
 from kubernetes.client import models as k8s
 
 # Env variables
-# TODO: remove the defaults
-DATA_PATH = os.getenv("DATA_PATH", "/app/artefacts/HousingData.csv")
-MLFLOW_TRACKING_URI = os.getenv(
-    "MLFLOW_TRACKING_URI", "http://host.docker.internal:5000"
-)
-DOCKER_REG_SECRET = os.getenv("DOCKER_REG_SECRET", "docker-registry-secret")
-NAMESPACE = os.getenv("NAMESPACE", "bridgeai")
-BASE_IMAGE = os.getenv("BASE_IMAGE", "renjithdigicat/bridgeai-regression:0.1")
-CONFIG_MAP = os.getenv("CONFIG_MAP", "training-config")
-# KUBECONFIG = os.getenv("KUBECONFIG", "~/.kube/config")
+"""
+# TODO: probably change this when dvc data versioning is available-
+# and pull the data directly from dvc remote without mounting the data path
+"""
+DATA_PATH = os.getenv("DATA_PATH")
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+DOCKER_REG_SECRET = os.getenv("DOCKER_REG_SECRET")
+NAMESPACE = os.getenv("NAMESPACE")
+BASE_IMAGE = os.getenv("BASE_IMAGE")
+CONFIG_MAP = os.getenv("CONFIG_MAP")
+CONNECTION_ID = os.getenv("CONNECTION_ID")
+KUBECONFIG = os.getenv("KUBECONFIG", "~/.kube/config")
+IN_CLUSTER = os.getenv("IN_CLUSTER", "False").lower() in ("true", "1", "t")
 
 
 @dag(schedule=None, catchup=False)
 def model_training_dag():
     """Model training dag."""
     # Define KubernetesPodOperator to run the training container
+    """
+    # TODO: Split this large task into 3 separate tasks
+    # preprocess >> train >> evaluate
+    """
     training_pod = KubernetesPodOperator(
-        task_id="regression_model_training_task",
-        name="regression-model-training",
+        kubernetes_conn_id=CONNECTION_ID,
         namespace=NAMESPACE,
         image=BASE_IMAGE,
+        task_id="regression_model_training_task",
+        name="regression-model-training",
         cmds=["poetry", "run", "python", "src/main.py"],
         image_pull_secrets=[k8s.V1LocalObjectReference(DOCKER_REG_SECRET)],
         env_vars={
@@ -37,25 +46,12 @@ def model_training_dag():
             "MLFLOW_TRACKING_URI": MLFLOW_TRACKING_URI,
         },
         volumes=[
-            # TODO: remove this temporary host data path mount
-            k8s.V1Volume(
-                name="host-data-volume",
-                host_path=k8s.V1HostPathVolumeSource(
-                    path="/mnt/data", type="Directory"
-                ),
-            ),
             k8s.V1Volume(
                 name="config-volume",
                 config_map=k8s.V1ConfigMapVolumeSource(name=CONFIG_MAP),
             ),
         ],
         volume_mounts=[
-            # TODO: remove this temporary data path mount
-            k8s.V1VolumeMount(
-                name="host-data-volume",
-                mount_path="/app/artefacts",
-                read_only=False,
-            ),
             k8s.V1VolumeMount(
                 name="config-volume",
                 mount_path="/config",
@@ -64,7 +60,7 @@ def model_training_dag():
         ],
         is_delete_operator_pod=True,
         get_logs=True,
-        in_cluster=True,  # If Airflow is running in cluster - set to True
+        in_cluster=IN_CLUSTER,
         config_file=KUBECONFIG,
     )
 
