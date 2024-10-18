abc856c41c42e41c1a3759641d3552c3d0efb7e4
Renjith Baby <139134056+renjith-digicat@users.noreply.github.com>
2024-10-18 10:23:52 +0100

    set pod resource constraints (#9)
    * set resource constraints
    
    * fix issues with setting resource constraints
    
    * fix issues with setting resource constraints
    
    * fix issues with setting resource constraints
    
    * fix issues with setting resource constraints
    
    * minor formatting
    
    * update readme
    
    * add varaible to enable resource constraints

--- dags/generate_model_image_to_deploy/create_model_image_to_deploy.py
+++ dags/generate_model_image_to_deploy/create_model_image_to_deploy.py
@@ -35,6 +35,42 @@ docker_push_secret_name = Variable.get(
     default_var="ecr-credentials",
 )
 
+enable_resource_constraints = Variable.get(
+    "docker_build_pod_resource_limits_enabled", default_var="True"
+).lower() in (
+    "true",
+    "1",
+    "t",
+)
+
+if enable_resource_constraints:
+    # Retrieve Airflow variables for resources
+    request_memory = Variable.get(
+        "docker_build_pod_request_memory", default_var="4Gi"
+    )
+    request_cpu = Variable.get("docker_build_pod_request_cpu", default_var="1")
+    request_eph_storage = Variable.get(
+        "docker_build_pod_request_eph_storage", default_var="8Gi"
+    )
+    limit_memory = Variable.get(
+        "docker_build_pod_limit_memory", default_var="30Gi"
+    )
+    limit_cpu = Variable.get("docker_build_pod_limit_cpu", default_var="2")
+
+    resources = k8s.V1ResourceRequirements(
+        requests={
+            "memory": request_memory,
+            "cpu": request_cpu,
+            "ephemeral-storage": request_eph_storage,
+        },
+        limits={
+            "memory": limit_memory,
+            "cpu": limit_cpu,
+        },
+    )
+else:
+    resources = None
+
 env_vars = [
     k8s.V1EnvVar(name="MLFLOW_TRACKING_URI", value=mlflow_tracking_uri),
     k8s.V1EnvVar(
@@ -119,6 +155,8 @@ def create_model_image_to_deploy_dag():
         image_pull_secrets=[k8s.V1LocalObjectReference(docker_reg_secret)],
         volume_mounts=[pvc_volume_mount, secret_volume_mount],
         volumes=[pvc_volume, secret_volume],
+        # Set resource constraints
+        container_resources=resources,
     )
 
     # Registering the task - Define the task dependencies here
