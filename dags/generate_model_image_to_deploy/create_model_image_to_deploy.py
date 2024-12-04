38a164adb35baa5f6c4f1f3b19b6b19b7e50db17
Renjith <renjith.baby@digicatapult.org.uk>
2024-12-04 11:16:25 +0000

    disable delete pod flag for all dags

--- dags/generate_model_image_to_deploy/create_model_image_to_deploy.py
+++ dags/generate_model_image_to_deploy/create_model_image_to_deploy.py
@@ -178,7 +178,7 @@ def create_model_image_to_deploy_dag():
         cmds=["poetry", "run", "python", "src/main.py"],
         image_pull_secrets=image_pull_secrets,
         env_vars=env_vars,
-        is_delete_operator_pod=True,
+        is_delete_operator_pod=False,
         get_logs=True,
         in_cluster=in_cluster,
         volume_mounts=[pvc_volume_mount],
@@ -199,7 +199,7 @@ def create_model_image_to_deploy_dag():
             f"--destination={docker_registry}/"
             f"{mlflow_built_image_name}:{mlflow_built_image_tag}",
         ],
-        is_delete_operator_pod=True,
+        is_delete_operator_pod=False,
         get_logs=True,
         in_cluster=in_cluster,
         image_pull_secrets=image_pull_secrets,
--- dags/regression_model_training/train_dag.py
+++ dags/regression_model_training/train_dag.py
@@ -143,7 +143,7 @@ def model_training_dag():
         env_vars=env_vars,
         volumes=[pvc_volume, config_volume],
         volume_mounts=[pvc_volume_mount, config_volume_mount],
-        is_delete_operator_pod=True,
+        is_delete_operator_pod=False,
         get_logs=True,
         in_cluster=in_cluster,
     )
@@ -159,7 +159,7 @@ def model_training_dag():
         env_vars=env_vars,
         volumes=[pvc_volume, config_volume],
         volume_mounts=[pvc_volume_mount, config_volume_mount],
-        is_delete_operator_pod=True,
+        is_delete_operator_pod=False,
         get_logs=True,
         in_cluster=in_cluster,
     )
@@ -175,7 +175,7 @@ def model_training_dag():
         env_vars=env_vars,
         volumes=[pvc_volume, config_volume],
         volume_mounts=[pvc_volume_mount, config_volume_mount],
-        is_delete_operator_pod=True,
+        is_delete_operator_pod=False,
         get_logs=True,
         in_cluster=in_cluster,
         do_xcom_push=True,
@@ -205,7 +205,7 @@ def model_training_dag():
         env_vars=env_vars,
         volumes=[pvc_volume, config_volume],
         volume_mounts=[pvc_volume_mount, config_volume_mount],
-        is_delete_operator_pod=True,
+        is_delete_operator_pod=False,
         get_logs=True,
         in_cluster=in_cluster,
     )
